50e39d0c32af750fcd90946a31c736fe02088e48
Pedro Marcos F <stack.peter21@gmail.com>
2021-06-16 11:50:28 -0300

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,10 @@ new SimpleSlide({
     auto: true, // se o slide deve passar automaticamente
     time: 5000, // tempo de transição dos slides
   });
+
+new SimpleSlide({
+    slide: 'produtos-2', // nome do atributo data-slide="principal"
+    nav: true, // se deve ou não mostrar a navegação
+    auto: true, // se o slide deve passar automaticamente
+    time: 10000, // tempo de transição dos slides
+  });
